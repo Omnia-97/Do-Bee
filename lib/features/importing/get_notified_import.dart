d8207e47e877ae421fc590191061b9c454c7b4a8
DELL <omnianabil1920@gmail.com>
2024-06-27 20:07:04 +0300

    adding importing screens

---
+++ lib/features/importing/get_notified_import.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GetNotifiedImport extends StatelessWidget {
+  const GetNotifiedImport({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
